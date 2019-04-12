a6b67fc21ce6c6d08a26d714762cb5636c9135ae
Robotanist <solomon2@hawaii.edu>
2019-04-12 01:07:40 -1000

    Phylogenetic Tree
    Creates an unrooted phylogenetic .tree file that can be opened in FIGTREE. Uses MUSCLE, Phangorn, DECIPHER packages and ShortRead from Bioconductor.

---
+++ phylogenetic_tree.R
@@ -0,0 +1,55 @@
+#Bioconductor has many packages for working with DNA sequences. Install the ShortRead package with
+
+#source("http://bioconductor.org/biocLite.R")
+#biocLite("ShortRead")
+
+library(ShortRead)
+library(DECIPHER)
+library(phangorn)
+library(muscle)
+
+# specify the path to the FASTA file (in quotes)
+fasta <- "/Users/solomon.champion/Desktop/FASTA/loci/rpl16/rpl16.fasta"
+
+# load the sequences from the file
+# change "DNA" to "RNA" or "AA" if necessary
+seqs <- readDNAStringSet(fasta)
+
+# look at some of the sequences (optional)
+seqs
+
+# nucleotide sequences need to be in the same orientation
+# if they are not, then they can be reoriented (optional)
+seqs <- OrientNucleotides(seqs)
+
+# perform alignment with MUSCLE
+aligned <- muscle(seqs)
+
+aligned <- as.phyDat(aligned, type = "DNA", levels = NULL, return.index = TRUE)
+
+# perform alignment with DECIPHER package
+# aligned <- AlignSeqs(seqs)
+
+# view the alignment in a browser (optional)
+# BrowseSeqs(aligned, highlight=0)
+
+# write the alignment to a new FASTA file (optional)
+# writeXStringSet(aligned, file="/Users/solomon.champion/Desktop/FASTA/loci/rpl16/rpl16_aligned.fasta")
+
+# calculates pairwise distance
+pairwise <- dist.ml(aligned, model = "F81", exclude = "none", bf = NULL, Q = NULL,
+                    k = 1L, shape = 1)
+
+# bootstrapping
+
+treeUPGMA  <- upgma(pairwise)
+
+fit <- pml(treeUPGMA, aligned)
+
+fit <- optim.pml(fit, rearrangements="NNI")
+set.seed(123)
+bs <- bootstrap.pml(fit, bs=100, optNni=TRUE)
+treeBS <- plotBS(fit$tree,bs)
+
+# open tree in FigTree
+write.tree(treeBS, "/Users/solomon.champion/Desktop/FASTA/loci/rpl16/rpl16.tree")
